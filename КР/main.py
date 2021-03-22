a905c9948539d15e9a8bde070ed3479ed9df1baf
vrs000 <vrs.my5@gmail.com>
2021-03-22 03:12:51 +0300

    update

--- КР/main.py
+++ КР/main.py
@@ -11,16 +11,20 @@ omega_list = []
 e_list = []
 tau_list = []
 
-
 a = get_a(ra=Earth_radius + h_a, rp=Earth_radius + h_p)
 e = get_e(ra=Earth_radius + h_a, rp=Earth_radius + h_p, a=a)
 p = get_p(a=a, e=e)
-omega = -1 #????
-tau = -1 #????
+omega = 0
+tau = 0
+
 
 def iterative_loop():
     for theta in theta_list:
         print(theta)
+
+
+def chart(x, y, title, xlabel, ylabel):
+    pass
 
 
 def main():
